2835e310549de214f77c65cb9c3d5e5ae78352fb
Ramses Lopez <ramses@theomx.com>
2018-10-05 23:03:54 -0400

    Use pg as database

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_225022) do
+ActiveRecord::Schema.define(version: 2018_10_06_024833) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "competitions", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "match_events", force: :cascade do |t|
+    t.bigint "match_id"
+    t.datetime "timestamp", default: -> { "CURRENT_TIMESTAMP" }
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["match_id"], name: "index_match_events_on_match_id"
   end
 
   create_table "matches", force: :cascade do |t|
